f912ff96d80c44836d96db709066beed9232a503
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-06-13 17:49:24 -0600

    fix: add template

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index ():
-    return "<h1>Hello Word</h1>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
